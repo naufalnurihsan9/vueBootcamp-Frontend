7091a15fd2e2005e356ce9a8a044cc5d94ca5771
Naufal Nur Ihsan <naufal.nurihsan@ai.astra.co.id>
2022-12-06 16:33:09 +0700

    Slicing HomePage Component

---
+++ src/components/HomePage/NewItems.vue
@@ -0,0 +1,40 @@
+<script setup>
+import { ref } from "vue";
+import NewItemsCard from "../NewItemsCard.vue";
+
+const items = ref([
+  {
+    id: 1,
+    title: "Mobile UI Kit",
+    description: "Slicing Mobile UI Kit",
+    image: "items-1.jpg",
+  },
+  {
+    id: 2,
+    title: "Online Doctor Consultation",
+    description: "Website UI Kit",
+    image: "items-2.jpg",
+  },
+  {
+    id: 3,
+    title: "Banking Crypto",
+    description: "Crypto Master",
+    image: "items-3.jpg",
+  },
+]);
+</script>
+
+<template>
+  <div class="container px-4 mx-auto my-16 md:px-12">
+    <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">New Items</h2>
+    <div class="flex flex-wrap -mx-1 lg:-mx-4">
+      <NewItemsCard
+        v-for="item in items"
+        :key="item.id"
+        :description="item.description"
+        :title="item.title"
+        :image="item.image"
+      />
+    </div>
+  </div>
+</template>
