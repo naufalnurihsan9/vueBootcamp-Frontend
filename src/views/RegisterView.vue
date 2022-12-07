d2737f0a7c80fa0041aa1fb4954ab6c5873f8d90
Naufal Nur Ihsan <Naufal.Ihsan@atlantica.dev>
2022-12-07 12:54:09 +0700

    Slicing Login & Register

---
+++ src/views/RegisterView.vue
@@ -0,0 +1,31 @@
+<script setup>
+import RegisterForm from "../components/authentication/RegisterForm.vue";
+import Feature from "../components/authentication/Feature/Feature.vue";
+</script>
+
+<template>
+  <main>
+    <div class="relative overflow-hidden bg-white">
+      <div class="mx-auto">
+        <div
+          class="flex flex-col items-center w-full min-h-screen pt-6 sm:justify-center sm:pt-0"
+        >
+          <div class="w-full p-5 mx-auto md:max-w-4xl">
+            <h2 class="mb-10 text-5xl font-bold text-center">
+              <span class="block lg:mb-2">Join & Get Our</span>
+              <span class="block">Pro Benefits</span>
+            </h2>
+          </div>
+          <div class="w-full p-5 mx-auto md:max-w-4xl">
+            <div class="grid grid-cols-1 gap-4 md:grid-cols-2">
+              <Feature />
+              <div>
+                <RegisterForm />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
